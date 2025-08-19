3697d6d9f0327ea1918855e3ccc1bdd6ceb34508
xfreddy2007 <xfreddy2007@google.com>
2025-08-19 14:22:54 +0800

    feat: update yarn version lock and clean up dummy page elements

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,4 @@
-import { nextJsConfig } from "@repo/eslint-config/next-js";
+import { nextJsConfig } from "@placement-io-oms/eslint-config/next-js";
 
 /** @type {import("eslint").Linter.Config} */
 export default nextJsConfig;
