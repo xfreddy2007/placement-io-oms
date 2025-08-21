fd990e7e3680a92b40929e25615032d25e8ce5a7
xfreddy2007 <xfreddy2007@google.com>
2025-08-21 16:39:50 +0800

    feat: add tailwind css library

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,7 @@
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}></main>
+    <div className="">
+      <main className="text-red-500">aaa</main>
     </div>
   );
 }
